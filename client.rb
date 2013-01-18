0a730980b27908661b3592f57546db240ca25414
Shintaro Tanaka <tanaka@hongo.wide.ad.jp>
2013-01-18 16:48:53 +0900

    classified server and client

--- client.rb
+++ client.rb
@@ -24,56 +24,100 @@ SERVER_ADDR = "127.0.0.1".freeze
 SERVER_PORT = 20000.freeze
 PROXY_PORT = 8080.freeze
 
-pipes = {}
+class NSAClient
+    def initialize(server_addr, server_port, listen_port)
+        @upstream_socket = TCPSocket.open(server_addr, server_port)
+        @listen_socket = TCPServer.open(listen_port)
+        @descriptors = [@upstream_socket, @listen_socket]
+        @ids = Hash.new
+    end # initialize
 
-sock_up = TCPSocket.open(SERVER_ADDR, SERVER_PORT)
-
-sock_listen = TCPServer.open(PROXY_PORT)
-
-socks = [sock_up, sock_listen]
-
-loop do
-    s = IO::select(socks)
-    s[0].each do |sock|
-        if sock === sock_listen then
-            # connection request. fork child process
-            client = sock_listen.accept
-            c_out, s_in = IO.pipe
-            s_out, c_in = IO.pipe
-            socks.push(s_out)
-
-            pid = fork do
-                client_id = pid
-                s = IO::select([c_out, client])
-                s[0].each do |sock|
-                    if sock === client then
-                        # ブラウザからのwriteは, session descriptorとflag byteを付加してserverへ送信
-                        data = sock.recv
-                        data = client_id.to_s + "\x00" + data
-                        sock_up.write(data)
+    def run
+        loop do
+            s = IO::select(@descriptors)
+            s[0].each do |sock|
+                if sock == @listen_socket then # 新しい接続要求
+                    accept_new_connection
+                elsif sock == @upstream_socket then # NSAServerからのresponse
+                    # そのままchild processに渡す
+                    data = sock.recv
+                    @ids[data.byteslice(0,2).to_i].write(data)
+                else # Browserからのrequest
+                    if sock.eof? then
+                        # disconnected. some procedure
+                        sock.close
+                        @descriptors.delete(sock)
                     else
-                        # pipeに対するIOは, descriptorとflag byteを取り除いてブラウザへ送信
-                        data = sock.recv
-                        data = data.byteslice(3, data.bytesize - 3)
-                        client.write(data)
+                        # session id は接続元port番号
+                        @upstream_socket.write sock.peeraddr[2] + "\x00" + sock.recv
                     end
                 end
-            end
-            pipes[pid] = [s_in, s_out]
+            end # each
+        end # loop
+    end # run
 
-        elsif sock === sock_up then
-            # descriptorを解析して, そのdescriptorで示されるs_inにdataを書き込む.
-            payload = sock.recv
-            desc = payload.byteslice(0, 2)
-            desc = desc.to_i
-            pipes[desc][0].write(payload)
-            # flagによってはprocessをkillし, pipeを破棄する
-        else
-            # そのままserverへpayloadをパスする
-            sock_up.write sock.recv
-        end
-    end
-end
+    private
 
-sock_listen.close
-sock_up.close
+    def accept_new_connection
+        newsock, sockaddr = @listen_socket.accept
+        @descriptors.push(newsock)
+        @ids[newsock.peeraddr[2] = newsock
+    end # accept_new_connection
+end # class NSAClient
+
+nsa_client = NSAClient.new(SERVER_ADDR, SERVER_PORT, PROXY_PORT)
+nsa_client.run
+
+#pipes = {}
+#
+#sock_up = TCPSocket.open(SERVER_ADDR, SERVER_PORT)
+#
+#sock_listen = TCPServer.open(PROXY_PORT)
+#
+#socks = [sock_up, sock_listen]
+#
+#loop do
+#    s = IO::select(socks)
+#    s[0].each do |sock|
+#        if sock === sock_listen then
+#            # connection request. fork child process
+#            client = sock_listen.accept
+#            c_out, s_in = IO.pipe
+#            s_out, c_in = IO.pipe
+#            socks.push(s_out)
+#
+#            pid = fork do
+#                client_id = pid
+#                s = IO::select([c_out, client])
+#                s[0].each do |sock|
+#                    if sock === client then
+#                        # ブラウザからのwriteは, session descriptorとflag byteを付加してserverへ送信
+#                        data = sock.recv
+#                        data = client_id.to_s + "\x00" + data
+#                        sock_up.write(data)
+#                    else
+#                        # pipeに対するIOは, descriptorとflag byteを取り除いてブラウザへ送信
+#                        data = sock.recv
+#                        data = data.byteslice(3, data.bytesize - 3)
+#                        client.write(data)
+#                    end
+#                end
+#            end
+#            pipes[pid] = [s_in, s_out]
+#
+#        elsif sock === sock_up then
+#            # descriptorを解析して, そのdescriptorで示されるs_inにdataを書き込む.
+#            payload = sock.recv
+#            desc = payload.byteslice(0, 2)
+#            desc = desc.to_i
+#            pipes[desc][0].write(payload)
+#            # flagによってはprocessをkillし, pipeを破棄する
+#        else
+#            # そのままserverへpayloadをパスする
+#            sock_up.write sock.recv
+#        end
+#    end
+#end
+#
+#sock_listen.close
+#sock_up.close
--- server.rb
+++ server.rb
@@ -1,15 +1,146 @@
-require 'eventmachine'
-require './handler.rb'
+# server.rb
+# 起動したら, nsa session用ソケットをlisten
+# clientの接続がアレばacceptとfork
+# 1. master process
+#   ソケットをselect
+#   書き込みがあったソケットが子プロセスとのpipeだった場合は, データにdescriptor番号を付加してnsa-serverへ送信
+#   書き込みソケットがnsa-serverとのコネクションだった場合は, descriptor番号を調べて適切な子プロセスのpipeへ書き込み
+# 2. child process
+#   ソケットをselect
+#   書き込みがあったソケットが
+#
+# ┌───────────┬─┬────────┐
+# │desc(2byte)│f│data    │
+# └───────────┴─┴────────┘
+# graceful close flag(0x001)が1ならば, requestキューが空の場合はコネクションをcloseしようとする
+# forceful close flag(0x010)が1ならば, 強制的にコネクションclose
+# response complete flag(0x100)が1ならば, requestキューから1つpopする
 
-host,port = "0.0.0.0", 8080
-puts "Starting server on #{host}:#{port}, #{EM::set_descriptor_table_size(32768)} sockets"
-EM.run do
-	Signal.trap("INT") { EM.stop }
-	Signal.trap("TERM") { EM.stop }
-	EM.start_server host, port, RequestHandler
-	if ARGV.size > 0
-		forks = ARGV[0].to_i
-		puts "... forking #{forks} times => #{2**forks} instances"
-		forks.times { fork }
-	end
-end
+require "socket"
+
+LISTEN_PORT = 20000.freeze
+
+class NSAWorker
+    def initialize(s)
+        @downstream_socket = s
+        @descriptors = [@downstream_socket]
+        @ids = Hash.new
+    end # initialize
+
+    def run
+        loop do
+            s = IO::select(@descriptors)
+            s[0].each do |sock|
+                if sock == @downstream_socket then
+                    # idチェック
+                    #   - 既存idの場合は@idsから使用ソケットを特定
+                    #   - 新規idの場合は, Request Stringから接続先ホスト, portを特定してconnection establish
+                    # CONNECTメソッドの場合
+                    #   - tunnel生成
+                    # それ以外の場合
+                    #   - Request Headerの書き換え
+                    #   - Request Stringのpath書き換え
+                    #   - 送信
+                else # from Origin Server
+                    if sock.eof? then
+                        sock.close
+                        @descriptors.delete(sock)
+                    else
+                        # Response Headerの書き換え
+                        # idをつけてNSA Sessionを通じてClientへ流す
+                    end
+                end
+            end # each
+        end # loop
+    end # run
+end # class NSAWorker
+
+class NSAServer
+    def initialize(listen_port)
+        @server_socket = TCPServer.open(listen_port)
+        @descriptors = [@server_socket]
+    end # initialize
+
+    def run
+        loop do
+            s = IO::select(@descriptors)
+            s[0].each do |sock|
+                if sock == @server_socket then
+                    accept_new_connection
+                end
+            end # each
+        end # loop
+    end
+
+    private
+
+    def accept_new_connection
+        newsock = @server_socket.accept
+        #@descriptors.push(newsock)
+        fork do
+            worker = NSAWorker.new(newsock)
+            worker.run
+        end
+    end # accept_new_connection
+end # NSAServer
+
+server = NSAServer.new(LISTEN_PORT)
+server.run
+
+#loop do
+#    s = IO::select(socks)
+#    s[0].each do |sock|
+#        if sock === sock_listen then
+#            # connection request. fork child process
+#            client = sock_listen.accept
+#
+#            fork do
+#                sock_client = client
+#                socks = [client]
+#                s = IO::select(socks)
+#                pipes = {}
+#                s[0].each do |sock|
+#                    if sock === sock_client then
+#                        # clientからのwriteは, session descriptorとflag byteをチェックしてworkerへ送信
+#                        data = sock.recv
+#                        desc = data.byteslice(0, 2)
+#                        desc = desc.to_i
+#                        begin
+#                            pipes[desc][0].write(data)
+#                        rescue
+#                            # 対応するworkerが無いので生成
+#                            w_out, m_in = IO.pipe
+#                            m_out, w_in = IO.pipe
+#                            socks.push(m_out)
+#                            fork do
+#                            end
+#                        end
+#                    else
+#                        # pipeに対するIOはそのままclientに送信
+#                        sock_client.write sock.recv
+#                    end
+#                end
+#            end
+#            pipes[pid] = [s_in, s_out]
+#
+#        elsif sock === sock_up then
+#            # descriptorを解析して, そのdescriptorで示されるs_inにdataを書き込む.
+#            data = sock.recv
+#            desc = data.byteslice(0, 2)
+#            desc = desc.to_i
+#            pipes[desc][0].write(data)
+#            # flagによってはprocessをkillし, pipeを破棄する
+#        else
+#            # そのままserverへpayloadをパスする
+#            sock_up.write sock.recv
+#        end
+#    end
+#    client = sock_listen.accept
+#    fork do
+#
+#    end
+#end
+#
+#sock.close
+#
+#sock_listen.close
